45b8807ea1fd15f397dc654e8007ed3c75074bea
caksodig <101535393+caksodig@users.noreply.github.com>
2022-05-19 23:28:57 +0700

    Polymorphysm

---
+++ Latihan1No5/VehicleNo5.java
@@ -0,0 +1,11 @@
+package AbstractClassInterfacePolymorphism.Latihan1No5;
+
+public abstract class VehicleNo5 {
+    public void function (){
+        System.out.println("Tools transportation");
+    }
+
+    public abstract void fuel ();
+
+    public abstract void walk();
+}
